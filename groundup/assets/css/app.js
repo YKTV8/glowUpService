5379ea51d98ff28dda1fc2a9298b60c163fee7aa
Geoffrey Matthews <97923330+YKTV8@users.noreply.github.com>
2023-05-25 03:42:50 -0500

    05/25
    style and js code for carousel

--- groundup/assets/css/app.js
+++ groundup/assets/css/app.js
@@ -31,3 +31,45 @@ hamburgerButton.addEventListener("click", function() {
     document.documentElement.style.height = ""; // Revert to default height when overlay is hidden
   }
 });
+// Code for Carousel Food Menu
+const body = document.body
+const slides = document.querySelectorAll('.carousel-item');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
+
+let activeSlide = 0
+
+rightBtn.addEventListener('click', () => {
+    activeSlide++
+
+    if (activeSlide > slides.length - 1) {
+        activeSlide = 0
+}
+setBgToBody()
+    setActiveSlide()
+})
+
+leftBtn.addEventListener('click', () => {
+    activeSlide--
+
+    if (activeSlide < 0) {
+        activeSlide = slides.length - 1
+    }
+
+    setBgToBody()
+    setActiveSlide()
+})
+
+setBgToBody()
+
+function setBgToBody(){
+    body.style.backgroundImage = slides[activeSlide].style.backgroundImage
+}
+
+function setActiveSlide(){
+    slides.forEach(slide => slide.classList.remove
+        ('active'))
+
+
+        slides[activeSlide].classList.add('active')
+    }
